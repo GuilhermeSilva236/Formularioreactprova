53c53c08132795a7d0ecc035fa6b1a62c7887857
unknown <22114290094@ACADEMICO.IESB.BR>
2023-05-22 22:07:58 -0300

    provafromularioorion

--- pages/cursos/[id].js
+++ pages/cursos/[id].js
@@ -1,16 +1,30 @@
 import Pagina from '@/components/Pagina'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
 import { BiCheckSquare, BiCaretLeft } from 'react-icons/bi'
 
-const index = () => {
+const form = () => {
 
-    const { push } = useRouter()
+    const { push, query } = useRouter()
+    const [curso, setCurso] = useState({})
+    const { register, handleSubmit, setValue } = useForm()
 
-    const { register, handleSubmit } = useForm()
+    useEffect(() => {
+
+      if(query.id){
+        const id = query.id
+        const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []
+        const curso = cursos[id]
+
+
+        for(let atributo in curso){
+            setValue(atributo, curso[atributo])
+        }
+      }
+    }, [query.id])
 
     function salvar(dados) { // salvar dados no localstorage 
         const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []// tirar de uma string 
@@ -55,4 +69,4 @@ const index = () => {
     )
 }
 
-export default index
+export default form
